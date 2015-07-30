22d6d870dc0f841c2deff6d44e5c2feaa9217633
mirkan <robin@bjornsvik.se>
2015-07-30 19:05:05 +0200

    improved out-folder

--- run.py
+++ run.py
@@ -7,9 +7,13 @@ and write the code below this text.
 """
 
 import sites
+import os.path
+import errno
 from subprocess import call
-out_folder = "$HOME/.local"
 
-#sites.wallhaven(out_folder)
-#call(["feh", "--randomize", "--bg-fill", out_folder])
-print (out_folder)
+# Path where images should be stored
+path = os.environ['HOME'] + "/.local/wallfetch"
+os.makedirs(path, exist_ok=True)
+
+sites.wallhaven(path)
+call(["feh", "--randomize", "--bg-fill", path])
